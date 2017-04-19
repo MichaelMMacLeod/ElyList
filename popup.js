e9d4bcc5f5fe5ca62aef96c680f61280d6224f75
Michael MacLeod <michaelmmacleod@gmail.com>
2017-04-19 14:36:20 -0700

    Make the text input field submit when the button/enter is pressed

--- popup.js
+++ popup.js
@@ -1,25 +1,7 @@
-popup = {
-    canvas: document.createElement('canvas'),
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('inputForm').addEventListener('submit', submit);
+});
 
-    width: 500,
-    height: 300,
-
-    start: function() {
-        // this.canvas.width = this.width;
-        // this.canvas.height = this.height;
-
-        // document.getElementById('canvas_container').appendChild(this.canvas);
-
-        // context = this.canvas.getContext('2d');
-        // context.fillStyle = 'black';
-        // context.fillRect(10, 10, 100, 100);
-
-        // this.interval = setInterval(this.update, 20);
-    },
-
-    update: function() {
-        console.log("hello world");
-    }
-};
-
-popup.start();
+function submit() {
+    
+}
