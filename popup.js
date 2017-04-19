182b0aea4e9cb30f6fb4ed92cb6f8596f4a7a3d5
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-04-18 22:28:03 -0700

    Add an "add task" button and input text field

--- popup.js
+++ popup.js
@@ -5,16 +5,16 @@ popup = {
     height: 300,
 
     start: function() {
-        this.canvas.width = this.width;
-        this.canvas.height = this.height;
+        // this.canvas.width = this.width;
+        // this.canvas.height = this.height;
 
-        document.getElementById('canvas_container').appendChild(this.canvas);
+        // document.getElementById('canvas_container').appendChild(this.canvas);
 
-        context = this.canvas.getContext('2d');
-        context.fillStyle = 'black';
-        context.fillRect(10, 10, 100, 100);
+        // context = this.canvas.getContext('2d');
+        // context.fillStyle = 'black';
+        // context.fillRect(10, 10, 100, 100);
 
-        this.interval = setInterval(this.update, 20);
+        // this.interval = setInterval(this.update, 20);
     },
 
     update: function() {
