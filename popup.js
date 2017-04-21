5c63fd5db44d3305e3247a23d7b81663c872aec1
Michael MacLeod <MichaelMMacLeod@gmail.com>
2017-04-20 19:40:37 -0700

    Properly add submitted input to the ordered task list

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
+inputTextField = document.getElementById('inputTextField');
+inputForm      = document.getElementById('inputForm');
+taskList       = document.getElementById('taskList');
+
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('inputForm').addEventListener('submit', function(e) {
-        document.getElementById('taskList').appendChild(document.createTextNode(e));
         e.preventDefault();
+
+        var node = document.createElement("LI");
+        var text = document.createTextNode(inputTextField.value);
+
+        node.appendChild(text);
+        taskList.appendChild(node);
+
+        inputTextField.value = "";
     });
 });
